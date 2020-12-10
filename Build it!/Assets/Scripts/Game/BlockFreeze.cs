4b217d865618bba852cdc2a38ea203c1677ffbe6
FrancescoVisco <francevisco@gmail.com>
2020-12-10 14:11:28 +0100

    Test Block Material

--- Build it!/Assets/Scripts/Game/BlockFreeze.cs
+++ Build it!/Assets/Scripts/Game/BlockFreeze.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class BlockFreeze : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
+    MeshRenderer m_MeshR;
+    public Material Material;
     
     void Start()
     {
        m_Rigidbody = GetComponent<Rigidbody>();
+       m_MeshR = GetComponent<MeshRenderer>();
     }
 
     void Update()
@@ -16,6 +19,7 @@ public class BlockFreeze : MonoBehaviour
       if(GameObject.Find("LevelGoal").GetComponent<Goal>().time < 0)
       {
         m_Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        m_MeshR.material = Material;
       }
     }
 }
